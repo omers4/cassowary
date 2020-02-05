957cc7f866ce6ceeb226af3ad0ebb2a650066fe8
Omer Shlifka <omershlifka@gmail.com>
2020-02-05 12:19:48 +0200

    try fixing travis CI

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='cassowary',
+    version= '0.1.0',
+    author='Omer Shlifka',
+    description='Final project in advanced system design course',
+    packages=find_packages(),
+)

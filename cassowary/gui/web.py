440bc5158aab2a50ee601979bcb9d2ccfa6655f0
Omer Shlifka <omershlifka@gmail.com>
2020-02-11 23:00:55 +0200

    gui: organized in grid. still need to centralize everything and to add graphs (?).

--- cassowary/gui/web.py
+++ cassowary/gui/web.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+import datetime
+import io
+from math import ceil
+
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -11,7 +15,30 @@ def register_views(db):
     @app.route('/users/<int:user_id>')
     def get_user_page(user_id):
         user = db.get_user(user_id)
+        birthday = datetime.datetime.fromtimestamp(user['birth'])
+        user['age'] = ceil((datetime.datetime.now() - birthday).days / 365)
         if user is None:
             return render_template('404.html')
         snapshots = db.get_user_snapshots_data(user_id)
+        for snapshot in snapshots:
+            timestamp = snapshot['timestamp']
+            snapshot['date'] = datetime.datetime.fromtimestamp(timestamp / 1000)
         return render_template('user.html', user=user, snapshots=snapshots)
+
+    @app.route('/users/<int:user_id>/snapshots/<int:snapshot_id>/<result_name>/data')
+    def get_user_snapshot_result_image(user_id, snapshot_id, result_name):
+        user = db.get_user(user_id)
+        if user is None:
+            return render_template('404.html')
+        snapshot = db.get_user_snapshot(user_id, snapshot_id)
+        if snapshot is None:
+            return render_template('404.html')
+        if result_name not in snapshot['results']:
+            return render_template('404.html')
+        if 'path' not in snapshot['results'][result_name]:
+            return render_template('404.html')
+
+        path = snapshot['results'][result_name]['path']
+        with open(path, 'rb') as image:
+            return send_file(io.BytesIO(image.read()),
+                             mimetype='image/png')
